1f94c9e70afea5a0c994a56b67e6d8e0b78c8083
n1xd <nikola.v.datkova@gmail.com>
2024-07-12 18:17:20 +0100

    des

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -10,7 +10,7 @@ const Home = () => {
         <video className="background-video" src={myvid} autoPlay loop muted />
         <div className="home-content-container">
           <div className="image-text-container">
-            <img src="/penn-logo.png" alt="Penn Logo" className="centered-image" />
+            <img src="../assets/penn-logo.png" alt="Penn Logo" className="centered-image" />
             <h1 className="penngustics-text">Penn Linguistics Society</h1>
           </div>
           <p className="centered-home-text">Welcome to Penn Linguistics Society (PLING) website! Find out more about us, browse events, and get involved in our community.</p>
