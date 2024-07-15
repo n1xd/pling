50e4572b393dd2545607dbb208fd602843fb4280
n1xd <nikola.v.datkova@gmail.com>
2024-07-15 23:41:52 +0100

    ferwq

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,9 +1,31 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import './Navbar.css';
 import logo from "../assets/intros/penn-logo.png";
 
 const Navbar = () => {
+    const [isMobileMenuOpen, setMobileMenuOpen] = useState(false);
+    const [isMobile, setIsMobile] = useState(false);
+
+    useEffect(() => {
+        const checkIfMobile = () => {
+            const isTouchDevice = 'ontouchstart' in window || navigator.maxTouchPoints > 0;
+            const isSmallScreen = window.innerWidth <= 768;
+            setIsMobile(isTouchDevice && isSmallScreen);
+        };
+
+        checkIfMobile();
+        window.addEventListener('resize', checkIfMobile);
+
+        return () => {
+            window.removeEventListener('resize', checkIfMobile);
+        };
+    }, []);
+
+    const handleMobileMenuToggle = () => {
+        setMobileMenuOpen(!isMobileMenuOpen);
+    };
+
     return (
         <nav className="navbar">
             <div className="navbar-logo">
@@ -11,16 +33,26 @@ const Navbar = () => {
                     <img src={logo} alt="Home" className="logo-image" />
                 </Link>
             </div>
-            <ul className="navbar-links">
-                <li><Link to="/about">About Us</Link></li>
-                <li><Link to="/events">Events</Link></li>
-                <li><Link to="/board">Board</Link></li>
-                <li><Link to="/contact">Contact Us</Link></li>
+            <ul className={`navbar-links ${isMobileMenuOpen ? 'open' : ''} ${isMobile ? 'mobile' : ''}`}>
+                <li><Link to="/about" onClick={handleMobileMenuToggle}>About Us</Link></li>
+                <li><Link to="/events" onClick={handleMobileMenuToggle}>Events</Link></li>
+                <li><Link to="/board" onClick={handleMobileMenuToggle}>Board</Link></li>
+                <li><Link to="/contact" onClick={handleMobileMenuToggle}>Contact Us</Link></li>
             </ul>
+            {isMobile && (
+                <div className="mobile-menu-icon" onClick={handleMobileMenuToggle}>
+                    <div className="line"></div>
+                    <div className="line"></div>
+                    <div className="line"></div>
+                </div>
+            )}
         </nav>
     );
 };
 
 export default Navbar;
+
+
+
 
 
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,20 +2,20 @@ import React from "react";
 import './Home.css';
 import logo from "../assets/intros/penn-logo.webp";
 
-
-
 const Home = () => {
   return (
-      <div className="home-container">
-        <video className="background-video" loading='lazy' src="https://github.com/n1xd/pling/raw/81778bb9f119b52d586e17767a9937daf5c58998/src/assets/intros/introvid.mp4" autoPlay loop muted />
-        <div className="home-content-container">
+    <div className="home-container">
+      <video className="background-video" loading='lazy' src="https://github.com/n1xd/pling/raw/81778bb9f119b52d586e17767a9937daf5c58998/src/assets/intros/introvid.mp4" autoPlay loop muted />
+      <div className="home-content-container">
+        <div className="image-text-wrapper">
           <div className="image-text-container">
             <img loading='lazy' src={logo} alt="Penn Logo" className="centered-image" />
             <h1 className="penngustics-text">Penn Linguistics Society</h1>
           </div>
-          <p className="centered-home-text">Welcome to the Penn Linguistics Society (PLING) website! Find out more about our student-run community, browse events, and share in our passion for language sciences.</p>
         </div>
+        <p className="centered-home-text">Welcome to the Penn Linguistics Society (PLING) website! Find out more about our student-run community, browse events, and share in our passion for language sciences.</p>
       </div>
+    </div>
   );
 };
 
